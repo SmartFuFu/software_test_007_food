7e262bbe71aa1e78d64251fcd8f57bf8463238e6
FU FUFU <126953223+SmartFuFu@users.noreply.github.com>
2024-06-10 16:40:06 +0800

    sub test code

---
+++ backend/src/test/java/org/tju/food_007/service/com/Information/CommodityInfomationServiceTest.java
@@ -0,0 +1,17 @@
+package org.tju.food_007.service.com.Information;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CommodityInfomationServiceTest {
+
+
+
+    @AfterEach
+    void tearDown() {
+    }
+}
